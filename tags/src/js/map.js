aae242d7703d68e77dd36f3f2343f461049e3c52
Vũ Sơn <sonvh2510@gmail.com>
2019-11-04 16:02:35 +0700

    update google map

--- tags/src/js/map.js
+++ tags/src/js/map.js
@@ -1,45 +1,38 @@
 const GGMapInit = () => {
 	const mapSelector = document.querySelector('#map');
 	const dealerLocatorList = document.querySelector('.dealer-locator-list .list');
-	
+
 	if (mapSelector) {
-		let map, markers = [], itemClicked;
-		const locations = locationsInput;
+		let map, markers = [],
+			itemClicked;
+		let locations = locationsInput;
 		const mapOption = {
 			zoom: 12,
-			styles: [
-				{
+			styles: [{
 					'featureType': 'administrative',
 					'elementType': 'labels.text.fill',
-					'stylers': [
-						{
-							'color': '#444444',
-						},
-					],
+					'stylers': [{
+						'color': '#444444',
+					}, ],
 				},
 				{
 					'featureType': 'landscape',
 					'elementType': 'all',
-					'stylers': [
-						{
-							'color': '#f2f2f2',
-						},
-					],
+					'stylers': [{
+						'color': '#f2f2f2',
+					}, ],
 				},
 				{
 					'featureType': 'poi',
 					'elementType': 'all',
-					'stylers': [
-						{
-							'visibility': 'off',
-						},
-					],
+					'stylers': [{
+						'visibility': 'off',
+					}, ],
 				},
 				{
 					'featureType': 'road',
 					'elementType': 'all',
-					'stylers': [
-						{
+					'stylers': [{
 							'saturation': -100,
 						},
 						{
@@ -50,35 +43,28 @@ const GGMapInit = () => {
 				{
 					'featureType': 'road.highway',
 					'elementType': 'all',
-					'stylers': [
-						{
-							'visibility': 'simplified',
-						},
-					],
+					'stylers': [{
+						'visibility': 'simplified',
+					}, ],
 				},
 				{
 					'featureType': 'road.arterial',
 					'elementType': 'labels.icon',
-					'stylers': [
-						{
-							'visibility': 'off',
-						},
-					],
+					'stylers': [{
+						'visibility': 'off',
+					}, ],
 				},
 				{
 					'featureType': 'transit',
 					'elementType': 'all',
-					'stylers': [
-						{
-							'visibility': 'off',
-						},
-					],
+					'stylers': [{
+						'visibility': 'off',
+					}, ],
 				},
 				{
 					'featureType': 'water',
 					'elementType': 'all',
-					'stylers': [
-						{
+					'stylers': [{
 							'color': '#0c6db5',
 						},
 						{
@@ -89,7 +75,7 @@ const GGMapInit = () => {
 			],
 		};
 		const infoWindow = new google.maps.InfoWindow();
-		
+
 		const addMarkers = () => {
 			const bounds = new google.maps.LatLngBounds();
 			locations.forEach((location, index) => {
@@ -104,12 +90,12 @@ const GGMapInit = () => {
 				markers.push(marker);
 				showInfoMarkerOnMap(marker, index);
 			});
-			
+
 			map.fitBounds(bounds);
 		};
-		
+
 		const showInfoMarkerOnMap = (marker, index) => {
-			google.maps.event.addListener(marker, 'click', function () {
+			google.maps.event.addListener(marker, 'click', function() {
 				infoWindow.setContent(`
 					<h3>${locations[index].name}</h3>
 					<p>${locations[index].address}</p>
@@ -121,7 +107,7 @@ const GGMapInit = () => {
 				map.setZoom(12);
 			})
 		};
-		
+
 		const getLocationList = () => {
 			if (dealerLocatorList) {
 				dealerLocatorList.innerHTML = '';
@@ -145,22 +131,21 @@ const GGMapInit = () => {
 				});
 			}
 		};
-		
+
 		const initialize = () => {
 			map = new google.maps.Map(mapSelector, mapOption);
-			
+
 			addMarkers();
-			
+
 			let listener = google.maps.event.addListener(map, 'idle', () => {
 				if (map.getZoom() > 12) {
 					map.setZoom(12);
 				}
 				google.maps.event.removeListener(listener);
 			});
-			
-			google.maps.event.addListener(map, 'bounds_changed', getLocationList);
 		};
 		
+		google.maps.event.addListener(map, 'bounds_changed', getLocationList);
 		google.maps.event.addDomListener(window, 'load', initialize);
 	}
 };
