a8124ecd46c8e063c97e5f89b3afe7113cf4afef
Vũ Sơn <sonvh2510@gmail.com>
2019-11-24 09:54:31 +0700

    update html, script, hiệu ứng

--- tags/ldp/src/js/main.js
+++ tags/ldp/src/js/main.js
@@ -92,22 +92,9 @@ const LandinPageSlider = () => {
 	let swiper = new Swiper('.ldp-banner .swiper-container', swiperOptions)
 }
 
-const WoWJS = () => {
-	const wow = new WOW({
-		boxClass: 'wow', // animated element css class (default is wow)
-		animateClass: 'animated', // animation css class (default is animated)
-		offset: 0, // distance to the element when triggering the animation (default is 0)
-		mobile: true, // trigger animations on mobile devices (default is true)
-		live: true, // act on asynchronously loaded content (default is true)
-		callback: function(box) {
-			// the callback is fired every time an animation is started
-			// the argument that is passed in is the DOM node being animated
-		},
-		scrollContainer: null, // optional scroll container selector, otherwise use window,
-		resetAnimation: true, // reset animation on end (default is true)
-	});
-	wow.init();
-}
+// const WoWJS = () => {
+// 	return 
+// }
 // ==> Call functions here
 document.addEventListener('DOMContentLoaded', () => {
 	Loading();
@@ -116,5 +103,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	ajaxPaginationProduct();
 	ajaxSelectProvince();
 	LandinPageSlider();
-	WoWJS();
+	new WOW({
+		boxClass: 'wow', // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset: 250, // distance to the element when triggering the animation (default is 0)
+		callback: function(box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+	}).init();
 });
